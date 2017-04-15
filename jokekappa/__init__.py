9e18153b3dddc6ce2088cc2662e66b0bbbfed882
Vinta <vinta.chen@gmail.com>
2017-04-16 03:46:57 +0800

    add noqa

--- jokekappa/__init__.py
+++ jokekappa/__init__.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from jokekappa.core import get_joke, get_jokes, update_jokes
+from jokekappa.core import get_joke, get_jokes, update_jokes  # noqa: F401
 
 
 __version__ = '0.1.4'
